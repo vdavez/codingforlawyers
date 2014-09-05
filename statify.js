fdf477aaafc11cc7c5b763bd634efd9b94893f38
V David Zvenyach <vdavez@gmail.com>
2014-09-05 14:43:04 -0400

    with content this time

--- statify.js
+++ statify.js
@@ -9,7 +9,7 @@ function makePages() {
  fs.readFile(__dirname + '/templates/_.base', {"encoding":"utf-8"}, function (err, site) {
   fs.readdir(__dirname + '/md/', function (err, files) {
    files.forEach(function (file, index, array) {
-    fs.readFile(__dirname + '/chapters/' + file, {"encoding":"utf-8", "flag":"r"}, function (err, data) {
+    fs.readFile(__dirname + '/md/' + file, {"encoding":"utf-8", "flag":"r"}, function (err, data) {
      marked(data, {gfm: false, breaks:false, renderer: new marked.Renderer()}, function (err, file_contents) {
       try {
         fs.mkdirSync(__dirname + '/chapters/'+ path.basename(file, '.md'));
