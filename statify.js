71b80146e459559e43aa78f5649fe987f5fa1e54
V David Zvenyach <vdavez@gmail.com>
2014-09-06 21:41:37 -0400

    allow for github editing

--- statify.js
+++ statify.js
@@ -6,6 +6,8 @@ var kramed = require('kramed');
 var path = require('path');
 var fs = require('fs');
 
+var gh_base = "https://github.com/vzvenyach/codingforlawyers/tree/gh-pages/md/"
+
 function makePages() {
  fs.readFile(__dirname + '/templates/_.base', {"encoding":"utf-8"}, function (err, site) {
   fs.readdir(__dirname + '/md/', function (err, files) {
@@ -15,9 +17,9 @@ function makePages() {
       try {
         fs.mkdirSync(__dirname + '/chapters/'+ path.basename(file, '.md'));
       }
-      catch (e) {        
+      catch (e) {
       }
-      fs.writeFile(__dirname + '/chapters/'+ path.basename(file, '.md') + '/index.html', swig.render(site, {autoescape: false, locals:{content:file_contents}}), function (err) {
+      fs.writeFile(__dirname + '/chapters/'+ path.basename(file, '.md') + '/index.html', swig.render(site, {autoescape: false, locals:{content:file_contents, gh_url: gh_base + path.basename(file)}}), function (err) {
        if (err) throw err;
        console.log(file + " converted")
       })
